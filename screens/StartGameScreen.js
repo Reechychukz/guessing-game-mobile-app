d0234c183359088f3a3b3917faa920875c09ac75
Richard Chukwuma <richardchukwuma99@gmail.com>
2022-07-24 23:48:27 +0100

    updated started game screen components

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
+import Card from "../components/Card";
+import Colors from "../constants/colors";
+
 const StartGameScreen = props => {
     return <View style={styles.screen}>
         <Text>Start A New Game!</Text>
-        <View style={styles.inputContainer}>
+        <Card style={styles.inputContainer}>
             <Text>Select A Number</Text>
             <TextInput />
             <View style={styles.buttonContainer}>
-                <Button title="Reset" onPress={() => { }} />
-                <Button title="Comfirm" onPress={() => { }} />
+                <View style={styles.button}>
+                    <Button title="Reset" onPress={() => { }} color={Colors.accent} />
+                </View>
+                <View style={styles.button}>
+                    <Button title="Comfirm" onPress={() => { }} color={Colors.primary} />
+                </View>
             </View>
-        </View>
+        </Card>
     </View>
 };
 
@@ -28,21 +35,16 @@ const styles = StyleSheet.create({
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems: 'center',
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 6,
-        shadowOpacity: 0.26,
-        elevation: 5,
-        backgroundColor: 'white',
-        padding: 20,
-        borderRadius: 10
+        alignItems: 'center'
     },
     buttonContainer: {
         flexDirection: 'row',
         width: '100%',
         justifyContent: 'space-between',
         paddingHorizontal: 15
+    },
+    button: {
+        width: 100
     }
 });
 
